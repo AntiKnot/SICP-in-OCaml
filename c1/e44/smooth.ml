2b390a6031e47dbd97bbe47fcee23116e8ee0fb7
Yansy <yansiyu.old.dog@hotmail.com>
2021-07-22 02:58:14 +0800

    add solution

---
+++ c1/e44/smooth.ml
@@ -0,0 +1,17 @@
+open Base
+
+let compose f g =  
+  fun x -> f (g x)
+
+let rec repeated f n = 
+  if n = 1 then f
+  else repeated (compose f f) (n-1)
+
+let dx = 0.00001
+let smooth f = 
+  fun x -> (f(x+.dx) +. f(x-.dx) +. f(x)) /. 3.0
+
+let smoonth_nth f n = 
+   repeated (smooth f) n
+
+  
