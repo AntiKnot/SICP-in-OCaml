ba821597109b04833a67a90b8f28aa98f3b1404d
Yansy <yansiyu.old.dog@hotmail.com>
2021-07-23 02:10:10 +0800

    add solution

--- c2/e4/ccc.ml
+++ c2/e4/ccc.ml
@@ -1,6 +1,18 @@
 open Base
 open Stdio
 
-let cons x y m = m x y
-let car z= z (fun p q -> p)
-let cdr z= z (fun p q -> q)
+let cons x y m = 
+  m x y
+let car z = 
+  (* z (fun p q -> p) *)
+  z (fun p _ -> p)
+let cdr z = 
+  (* z (fun p q -> q) *)
+  z (fun _ q -> q)
+
+(* Returns a partial function, ready to accept a function parameter,
+This function will be called with the first two parameters as parameters 
+cons 1 2 -> fun m -> m 1 2
+car (cons 1 2) -> (fun p _ -> p) 1 2 -> 1
+cdr (cons 1 2) -> (fun p _ -> p) 1 2 -> 2
+*)
