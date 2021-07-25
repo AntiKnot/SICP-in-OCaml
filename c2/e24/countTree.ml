ab0ef49d95755de27d09275d6c694feec4dec74b
Yansy <yansiyu.old.dog@hotmail.com>
2021-07-25 19:16:18 +0800

    add solution

---
+++ c2/e24/countTree.ml
@@ -0,0 +1,31 @@
+(* (cons (list 1 2) (list 3 4))
+The structure I am looking for is ((1 2)(3 4))
+But the actual structure is ((1 2) 3 4) *)
+open Base
+open Stdio
+
+(* Build the structure of a binary tree. *)
+(* type  'a tree = 
+| Leaf
+| Node of 'a * 'a tree * 'a tree
+
+let t = 
+  Node (4,
+    Node(2,
+    Node(1,Leaf,Leaf),
+    Node(3,Leaf,Leaf)
+    ),
+    Node(5,
+      Node(6,Leaf,Leaf),
+      Node(7,Leaf,Leaf)
+    )
+  )
+
+let rec size = function
+| Leaf -> 0
+| Node  (_,l,r) -> 1+ size l + size r
+
+let () = 
+  printf "%N\n" (size t);;
+ *)
+
